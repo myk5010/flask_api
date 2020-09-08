ec83d34d6cf9fa9f0efac1ba2629e16752f8e710
maiyongke <273775054@qq.com>
2020-09-08 16:52:18 +0800

    删除了所有的 postgres 配置，以支持 sqlite 的配置

--- app/config.py
+++ app/config.py
@@ -1,47 +1,26 @@
 import os
 
-POSTGRES = {
-    'user': os.getenv('POSTGRES_USER', 'postgres'),
-    'pw': os.getenv('POSTGRES_PASSWORD', 'root'),
-    'db': os.getenv('POSTGRES_DB', 'postgres'),
-    'host': os.getenv('POSTGRES_HOST', 'localhost'),
-    'port': os.getenv('POSTGRES_PORT', 5432),
-}
-
-TEST_POSTGRES = {
-    'user': os.getenv('POSTGRES_USER', 'postgres'),
-    'pw': os.getenv('POSTGRES_PASSWORD', 'root'),
-    'db': os.getenv('POSTGRES_DB', 'postgres'),
-    'host': os.getenv('POSTGRES_HOST', 'localhost'),
-    'port': os.getenv('POSTGRES_PORT', 5432),
-}
-
-
 class Config:
     ERROR_404_HELP = False
 
     SECRET_KEY = os.getenv('APP_SECRET', 'secret key')
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{host}:{port}/{db}'.format(**POSTGRES)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///tutorial.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     DOC_USERNAME = 'api'
     DOC_PASSWORD = 'password'
 
-
 class DevConfig(Config):
     DEBUG = True
 
-
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{host}:{port}/{db}'.format(**TEST_POSTGRES)
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
     TESTING = True
     DEBUG = True
 
-
 class ProdConfig(Config):
     DEBUG = False
-
 
 config = {
     'development': DevConfig,
